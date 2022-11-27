4227367f2332586c66ac395d669d995f8a0bb16d
Greg C <gregjc18@gmail.com>
2022-11-27 22:27:17 +1300

    Get proper file names and also save internet shortcut

--- blendermarket.py
+++ blendermarket.py
@@ -20,22 +20,25 @@ def listOrders() -> [str]:
     orders = [x.parent.get('href') for x in body.select("td > a > i.fa-download")]
     return orders
 
-def getPurchasesFromOrder(url: str) -> (str, [(str, str)]):
+def getPurchasesFromOrder(url: str) -> (str, str, [(str, str)]):
     body = fetch(url)
 
     rows = body.select('div.card-body.row')
     for row in rows:
-        name = [x.text for x in row.select('div:nth-child(1) a:nth-child(2)')][0]
+        title = row.select('div:nth-child(1) h5 a')[0]
+
+        name = title.text
+        store_page = "https://blendermarket.com" + title.attrs['href']
         files = [(x.text, 'https://blendermarket.com' + x.get('href')) for x in row.select('div:nth-child(2) li>a')]
 
-        yield (name, files)
+        yield (name, store_page, files)
 
 def fetch(url: str) -> bs4.BeautifulSoup:
     r = requests.get(url, cookies=cookies.cookies)
     if r.status_code != 200:
         return None
 
-    body = BeautifulSoup(r.text)
+    body = BeautifulSoup(r.text, features="html.parser")
     if not checkLoggedIn():
         return None
 
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import re
 import requests
 import subprocess
 import cookies
+from uuid import uuid4
 
 parser = argparse.ArgumentParser(
     prog = "BlenderMarket Downloader",
@@ -26,13 +27,22 @@ def main():
 
     for x in purchases:
         addon_name = x[0]
-        assets = x[1]
+        store_page = x[1]
+        assets = x[2]
 
 
         folder_name = re.sub(r'[|.,+!@&*:;=&^%$#]+', '', addon_name)
         folder_name = re.sub(r'[\s\-]+', '-', folder_name)
+        folder_name = re.sub(r'^[\s\-]+', '', folder_name)
         path = os.path.join(args.destination, folder_name)
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+        with open(os.path.join(path, 'blendermarket.url'), 'w') as fd:
+            fd.write("[{" + str(uuid4()) + "}]\n")
+            fd.write(f"Prop3 = 19, 2\n")
+            fd.write(f"[InternetShortcut]]\n")
+            fd.write(f"IDList =\n")
+            fd.write(f"URL = {store_page}")
 
         for asset in assets:
             asset_name = re.sub(r'\s+', '-', asset[0])
@@ -43,8 +53,17 @@ def main():
                 print(f"skipping '{asset_name}' because it already exists in {asset_path}")
                 continue
 
+            # [{000214A0 - 0000 - 0000 - C000 - 000000000046}]
+            # Prop3 = 19, 2
+            # [InternetShortcut]
+            # IDList =
+            # URL = http: // www.google.com /
+
+
+
             print(f"downloading '{asset_name}' to {asset_path}")
             r = requests.get(url, cookies=cookies.cookies)
+
             r = requests.get(r.url)
             with open(asset_path, 'wb') as fd:
                 for chunk in r.iter_content(chunk_size=128):
